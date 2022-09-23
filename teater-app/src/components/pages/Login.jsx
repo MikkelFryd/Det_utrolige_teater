da5b2e0f648cedcc7af9a4bcde06e2d011843e6c
mikk7021 <mikk7021@gmail.com>
2022-09-23 13:11:27 +0200

    Added shop, fixes

--- teater-app/src/components/pages/Login.jsx
+++ teater-app/src/components/pages/Login.jsx
@@ -2,100 +2,105 @@ import { useAuth } from "../context/authcontext/Auth";
 import { useFavorites } from "../context/favoritecontext/Favorite";
 import { Footer } from "../partials/footer/Footer";
 import { Header } from "../partials/header/Header";
-import Style from './login.module.scss'
-import LoginHeart from '../../assets/images/LoginHeart.svg'
+import Style from "./login.module.scss";
+import LoginHeart from "../../assets/images/LoginHeart.svg";
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const Login = () => {
-
   const { loginData, setLoginData } = useAuth();
 
-  const { favorites } = useFavorites()
+  const { favorites } = useFavorites();
 
-  const [reviewData, setReviewData] = useState([])
+  const [reviewData, setReviewData] = useState([]);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const sortData = () => {
-
-    const myReviews = reviewData.filter(item => item.user_id === 39);
-
-    console.log(myReviews)
-  }
-
-  console.log(reviewData.includes())
-  
   const logOut = () => {
     sessionStorage.removeItem("token");
     setLoginData("");
-    navigate('/')
+    navigate("/");
   };
 
   useEffect(() => {
     const getReviewData = async () => {
-        let result = await axios.get('https://api.mediehuset.net/detutroligeteater/reviews')
-        setReviewData(result.data.items)
-        sortData()
-    }
-    getReviewData()
-}, [])
+      let result = await axios.get(
+        "https://api.mediehuset.net/detutroligeteater/reviews"
+      );
+      setReviewData(result.data.items);
+    };
+    getReviewData();
+  }, []);
 
+  const handleDelete = async (item) => {
+    const options = {
+      headers: {
+        Authorization: `Bearer ${loginData.access_token}`,
+      },
+    };
+
+    let result = await axios.delete(
+      `https://api.mediehuset.net/detutroligeteater/favorites/${item.event_id}`,
+      options
+    );
+    console.log(result);
+  };
 
   return (
     <>
-    <Header />
-      {loginData && loginData.username ? 
-      <section className={Style.logincontainer}>
-        <div className={Style.topcontainer}>
+      <Header />
+      {loginData && loginData.username ? (
+        <section className={Style.logincontainer}>
+          <div className={Style.topcontainer}>
             <h1>Min side</h1>
             <div>
-                <p>DU ER LOGOET PÅ {loginData.username}</p>
-                <button onClick={logOut}>LOG OUT</button>
+              <p>DU ER LOGOET PÅ {loginData.username}</p>
+              <button onClick={logOut}>LOG OUT</button>
             </div>
-        </div>
-        <article>
+          </div>
+          <article>
             <div>
-                <h2>MINE RESERVATIONER</h2>
+              <h2>MINE RESERVATIONER</h2>
             </div>
-        </article>
-        <article>
+          </article>
+          <article>
             <div className={Style.articlewrapper}>
-                <div className={Style.gridcontainer}>
-                    <img src={LoginHeart} alt="icon" />
-                    <h2>MINE FAVORITTER</h2>
-                </div>
-                <div className={Style.gridwrapper}>
-                    <h5>FORESTILLING</h5>
-                    <h5>REDIGER</h5>
-                </div>
-                {favorites.length > 1 ? 
+              <div className={Style.gridcontainer}>
+                <img src={LoginHeart} alt="icon" />
+                <h2>MINE FAVORITTER</h2>
+              </div>
+              <div className={Style.gridwrapper}>
+                <h5>FORESTILLING</h5>
+                <h5>REDIGER</h5>
+              </div>
+              {favorites.length > 1 ? (
                 <div className={Style.favoritecontainer}>
-                {favorites.map((item, index) => {
+                  {favorites.map((item, index) => {
                     return (
-                        <div key={index}>
-                            <p>{item.title}, {item.stage_name}</p>
-                            <button></button>
-                        </div>
-                    )
-                })}
-                </div> : null}
+                      <div key={index}>
+                        <p>
+                          {item.title}, {item.stage_name}
+                        </p>
+                        <button onClick={() => handleDelete(item)}></button>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : null}
             </div>
-        </article>
-        <article>
+          </article>
+          <article>
             <div>
-                <h4>MINE ANMELDELSER</h4>
-                {reviewData && reviewData.map((item, index) => {
-                    return (
-                        <>
-                        </>
-                    )
+              <h4>MINE ANMELDELSER</h4>
+              {reviewData &&
+                reviewData.map((item, index) => {
+                  return <></>;
                 })}
             </div>
-        </article>
-      </section> : null        
-      }
+          </article>
+        </section>
+      ) : null}
       <Footer />
     </>
   );
